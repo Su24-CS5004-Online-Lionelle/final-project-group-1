73f295cf7dfea11547d1ba8c0228c821c7f14888
pfochios <fochios.p@northeastern.edu>
2024-07-27 18:23:16 -0400

    created javadocs for breed record

--- src/main/java/Breed.java
+++ src/main/java/Breed.java
@@ -1,5 +1,22 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+/**
+ * Represents a dog breed with its characteristics.
+ * This record encapsulates various attributes of a dog breed, including
+ * identification, physical characteristics, and hypoallergenic status.
+ *
+ * @param id The unique identifier for the breed.
+ * @param name The name of the breed.
+ * @param description A brief description of the breed's characteristics.
+ * @param lifeMin The minimum life expectancy of the breed in years.
+ * @param lifeMax The maximum life expectancy of the breed in years.
+ * @param maleWeightMin The minimum weight of male dogs of this breed in pounds.
+ * @param maleWeightMax The maximum weight of male dogs of this breed in pounds.
+ * @param femaleWeightMin The minimum weight of female dogs of this breed in pounds.
+ * @param femaleWeightMax The maximum weight of female dogs of this breed in pounds.
+ * @param hypoallergenic Indicates whether the breed is considered hypoallergenic.
+ */
 public record Breed (
 
     String id,
