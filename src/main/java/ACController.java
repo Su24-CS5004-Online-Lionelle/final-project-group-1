8fa5a178da396f6ae5b7d59af4424cadfa94bbff
pfochios <fochios.p@northeastern.edu>
2024-07-30 15:37:31 -0400

    created Jsonformat converter method and added javadocs for ACController class

--- src/main/java/ACController.java
+++ src/main/java/ACController.java
@@ -1,61 +1,118 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.List;
 import java.util.stream.Stream;
 
 public class ACController {
+    /** The model containing adoption center data. */
     private AdoptionCenterModel model;
+    /** The list of dogs resulting from applied filters. */
     private List<Dog> resultList;
+    /** The planner for filtering dogs. */
     private ACFilterPlanner planner;
+    /** Flag indicating if name filter is active. */
     private boolean nameOn = false;
+    /** The search string for filtering by name. */
     private String nameSearch;
+    /** Flag indicating if sex filter is active. */
     private boolean sexOn = false;
+    /** The search string for filtering by sex. */
     private String sexSearch;
+    /** Flag indicating if breed filter is active. */
     private boolean breedOn = false;
+    /** The search string for filtering by breed. */
     private String breedSearch;
+    /** Flag indicating if age filter is active. */
     private boolean ageOn = false;
+    /** The search string for filtering by age. */
     private String ageSearch;
+    /** Flag indicating if weight filter is active. */
     private boolean weightOn = false;
+    /** The search string for filtering by weight. */
     private String weightSearch;
+    /** Flag indicating if price filter is active. */
     private boolean priceOn = false;
+    /** The search string for filtering by price. */
     private String priceSearch;
+    /** The attribute to sort results by. */
     private String sortOn = "price";
+    /** Flag indicating if sorting should be in ascending order. */
     private boolean ascending = true;
 
-    
+    /**
+     * Constructs a new ACController with the given model.
+     *
+     * @param model The AdoptionCenterModel to use.
+     */
     public ACController(AdoptionCenterModel model) {
         this.model = model;
         this.ACFilterPlanner = new ACFilterPlanner(model.getAdoptableDogs());
     }
 
+    /**
+     * Sets the name search filter.
+     *
+     * @param name The name to search for.
+     */
     public void setNameSearch(String name) {
         this.nameOn = true;
         this.nameSearch = name;
     }
 
+    /**
+     * Sets the sex search filter.
+     *
+     * @param sex The sex to search for.
+     */
     public void setSexSearch(String sex) {
         this.sexOn = true;
         this.sexSearch = sex;
     }
 
+    /**
+     * Sets the breed search filter.
+     *
+     * @param breed The breed to search for.
+     */
     public void setBreedSearch(String breed) {
         this.breedOn = true;
         this.breedSearch = breed;
     }
 
+    /**
+     * Sets the age search filter.
+     *
+     * @param age The age to search for.
+     */
     public void setAgeSearch(String age) {
         this.ageOn = true;
         this.ageSearch = age;
     }
 
+    /**
+     * Sets the weight search filter.
+     *
+     * @param weight The weight to search for.
+     */
     public void setWeightSearch(String weight) {
         this.weightOn = true;
         this.weightSearch = weight;
     }
 
+    /**
+     * Sets the price search filter.
+     *
+     * @param price The price to search for.
+     */
     public void setPriceSearch(String price) {
         this.priceOn = true;
         this.priceSearch = price;
     }
 
+    /**
+     * Clears all search filters.
+     */
     public void clearSearchFilters() {
         nameOn = false;
         sexOn = false;
@@ -65,8 +122,11 @@ public class ACController {
         priceOn = false;
     }
 
+    /**
+     * Applies all active filters and sets the resultList.
+     */
     public void setResultList() {
-        Stream filteredDogs;
+        Stream<Dog> filteredDogs;
         filteredDogs = planner.filter(
         this.nameOn, this.nameSearch,
         this.sexOn, this.sexSearch,
@@ -75,6 +135,24 @@ public class ACController {
         this.weightOn, this.weightSearch,
         this.priceOn, this.priceSearch, this.sortOn, this.ascending);
         this.resultList = filteredDogs.toList();
+    }
+
+    /**
+     * Converts the resultList to JSON format.
+     *
+     * @return A JSON string representation of the resultList.
+     */
+    public String getResultListToJsonFormat() {
+
+        setResultList();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(this.resultList);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "[]";
+        }
     }
 
 //    public List<Dog> getResultList() {
