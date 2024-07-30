2b9d1ed70406f05de36b1253ba0e1f5b176df39b
jowxu <jxu2193@gmail.com>
2024-07-30 13:51:03 -0400

    Update ACController.java

--- src/main/java/ACController.java
+++ src/main/java/ACController.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class ACController {
+    private AdoptionCenterModel model;
+    private List<Dog> resultList;
+    private ACFilterPlanner planner;
+    
+    public ACController(AdoptionCenterModel model) {
+        this.model = model;
+        this.ACFilterPlanner = new ACFilterPlanner(model.getAllDogs());
+    }
+
+    public void setResultList(String userInput) {
+        this.resultList = ACFilterPlanner(userInput);
+    }
 }
