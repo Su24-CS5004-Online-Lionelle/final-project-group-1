32c225f9389e08ae72bd2b2caf9ea60caec0ab77
pfochios <fochios.p@northeastern.edu>
2024-07-30 14:44:39 -0400

    Creating methods for controller

--- src/main/java/ACController.java
+++ src/main/java/ACController.java
@@ -21,4 +21,48 @@ public class ACController {
         this.model = model;
         this.ACFilterPlanner = new ACFilterPlanner(model.getAdoptableDogs());
     }
+
+    public void setNameSearch(String name) {
+        this.nameOn = true;
+        this.nameSearch = name;
+    }
+
+    public void setSexSearch(String sex) {
+        this.sexOn = true;
+        this.sexSearch = sex;
+    }
+
+    public void setBreedSearch(String breed) {
+        this.breedOn = true;
+        this.breedSearch = breed;
+    }
+
+    public void setAgeSearch(String age) {
+        this.ageOn = true;
+        this.ageSearch = age;
+    }
+
+    public void setWeightSearch(String weight) {
+        this.weightOn = true;
+        this.weightSearch = weight;
+    }
+
+    public void setPriceSearch(String price) {
+        this.priceOn = true;
+        this.priceSearch = price;
+    }
+
+    public void clearSearchFilters() {
+        nameOn = false;
+        sexOn = false;
+        breedOn = false;
+        ageOn = false;
+        weightOn = false;
+        priceOn = false;
+    }
+
+    public List<Dog> getResultList() {
+
+    }
+
 }
