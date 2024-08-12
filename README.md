874c7abae55f32eb9c52c0b33ccaa0b22cc1fa5f
pfochios <fochios.p@northeastern.edu>
2024-08-11 23:14:58 -0400

    cleaned up formatting for DogAdoptionGUI and added in my link for my github on the README.md doc

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 * The group 1 names and Github URLs
   * Dina Barua - 
   * Zheqi(Emily) Liu - https://github.com/emiiiliu
-  * Philip Fochios - 
+  * Philip Fochios - https://github.com/pfochios
   * Joel Xu - https://github.com/jowxu
   
 * D.E.J.P's Adoption Center Program: A program that allows the user to view a collection of adoptable dogs from our shelter. The user will be able to filter and sort through the dogs based off of certain dog details and then be able to add whichever dog they want to a wishlist. The user will also be able to save their wishlist to a JSON file.
--- src/main/java/view/DogAdoptionGUI.java
+++ src/main/java/view/DogAdoptionGUI.java
@@ -206,14 +206,19 @@ public class DogAdoptionGUI extends JFrame {
             updateDogList(resultList); // Update the UI with the filtered list
 
             if (resultList.isEmpty()) {
-                JOptionPane.showMessageDialog(this, "No dogs match the search criteria.", "Search Results", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(this,
+                        "No dogs match the search criteria.", "Search Results",
+                        JOptionPane.INFORMATION_MESSAGE);
             } else {
-                JOptionPane.showMessageDialog(this, resultList.size() + " dogs found matching the search criteria.", "Search Results", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(this,
+                        resultList.size() + " dogs found matching the search criteria.",
+                        "Search Results", JOptionPane.INFORMATION_MESSAGE);
             }
 
 
         } catch (IllegalArgumentException e) {
-            JOptionPane.showMessageDialog(this, e.getMessage(), "Invalid Input", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this, e.getMessage(),
+                    "Invalid Input", JOptionPane.ERROR_MESSAGE);
         }
     }
 
@@ -259,7 +264,8 @@ public class DogAdoptionGUI extends JFrame {
 
         // Load and display the image
         ImageIcon imageIcon = new ImageIcon(dog.getImage());
-        Image image = imageIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        Image image = imageIcon.getImage().getScaledInstance
+                (100, 100, Image.SCALE_SMOOTH);
         JLabel imageLabel = new JLabel(new ImageIcon(image));
         panel.add(imageLabel, BorderLayout.CENTER);
 
@@ -271,7 +277,8 @@ public class DogAdoptionGUI extends JFrame {
         panel.add(infoLabel, BorderLayout.SOUTH);
 
         // Add/Remove from wishlist button
-        JButton wishlistButton = new JButton(controller.getWishList().contains(dog) ? "Remove from Wishlist" : "Add to Wishlist");
+        JButton wishlistButton = new JButton(controller.getWishList().contains(dog)
+                ? "Remove from Wishlist" : "Add to Wishlist");
         wishlistButton.addActionListener(e -> toggleWishlist(dog, wishlistButton));
         panel.add(wishlistButton, BorderLayout.NORTH);
 
@@ -333,7 +340,8 @@ public class DogAdoptionGUI extends JFrame {
                 filePath += ".json";
             }
             controller.saveList(filePath); // Save the wishlist to the specified file
-            JOptionPane.showMessageDialog(this, "Wishlist saved successfully to " + filePath);
+            JOptionPane.showMessageDialog(this,
+                    "Wishlist saved successfully to " + filePath);
         }
     }
 
@@ -350,36 +358,94 @@ public class DogAdoptionGUI extends JFrame {
         Map<String, Breed> breeds;
         try {
             breeds = util.parseBreeds(util.getBreeds());
-            Dog dog1 = new Dog("1", "Alexander", "M", breeds.get("Caucasian Shepherd Dog"), 24, 12.0, "1.png");
-            Dog dog2 = new Dog("2", "Bella", "F", breeds.get("Bouvier des Flandres"), 3, 65.0, "2.png", 1200.00, true);
-            Dog dog3 = new Dog("3", "Charlie", "M", breeds.get("Hanoverian Scenthound"), 5, 70.0, "3.png", 1500.00, true);
-            Dog dog4 = new Dog("4", "Daisy", "F", breeds.get("Tibetan Spaniel"), 4, 60.0, "4.png", 1300.00, true);
-            Dog dog5 = new Dog("5", "Ella", "F", breeds.get("Border Collie"), 2, 25.0, "5.png", 800.00, true);
-            Dog dog6 = new Dog("6", "Finn", "M", breeds.get("Curly-Coated Retriever"), 6, 50.0, "6.png", 900.00, true);
-            Dog dog7 = new Dog("7", "Ginger", "F", breeds.get("Skye Terrier"), 7, 55.0, "7.png", 1100.00, true);
-            Dog dog8 = new Dog("8", "Hunter", "M", breeds.get("Hokkaido"), 3, 85.0, "8.png", 1700.00, true);
-            Dog dog9 = new Dog("9", "Ivy", "F", breeds.get("Hokkaido"), 4, 45.0, "9.png", 1400.00, true);
-            Dog dog10 = new Dog("10", "Jack", "M", breeds.get("Japanese Terrier"), 5, 75.0, "10.png", 1600.00, true);
-            Dog dog11 = new Dog("11", "Kira", "F", breeds.get("Caucasian Shepherd Dog"), 6, 65.0, "11.png", 1200.00, true);
-            Dog dog12 = new Dog("12", "Leo", "M", breeds.get("Bouvier des Flandres"), 2, 20.0, "12.png", 700.00, true);
-            Dog dog13 = new Dog("13", "Molly", "F", breeds.get("Hanoverian Scenthound"), 7, 15.0, "13.png", 900.00, true);
-            Dog dog14 = new Dog("14", "Nala", "F", breeds.get("Tibetan Spaniel"), 5, 140.0, "14.png", 0, false);
-            Dog dog15 = new Dog("15", "Oscar", "M", breeds.get("Border Collie"), 3, 8.0, "15.png", 600.00, true);
-            Dog dog16 = new Dog("16", "Penny", "F", breeds.get("Curly-Coated Retriever"), 4, 5.0, "16.png", 800.00, true);
-            Dog dog17 = new Dog("17", "Quinn", "F", breeds.get("Skye Terrier"), 6, 12.0, "17.png", 1000.00, true);
-            Dog dog18 = new Dog("18", "Rocky", "M", breeds.get("Tibetan Spaniel"), 5, 60.0, "18.png", 1300.00, true);
-            Dog dog19 = new Dog("19", "Sophie", "F", breeds.get("Hokkaido"), 7, 7.0, "19.png", 850.00, true);
-            Dog dog20 = new Dog("20", "Toby", "M", breeds.get("Japanese Terrier"), 2, 30.0, "20.png", 1100.00, true);
-            Dog dog21 = new Dog("21", "Ursula", "F", breeds.get("Caucasian Shepherd Dog"), 3, 70.0, "21.png", 1500.00, true);
-            Dog dog22 = new Dog("22", "Victor", "M", breeds.get("Bouvier des Flandres"), 6, 80.0, "22.png", 1700.00, true);
-            Dog dog23 = new Dog("23", "Wendy", "F", breeds.get("Hanoverian Scenthound"), 4, 65.0, "23.png");
-            Dog dog24 = new Dog("24", "Xander", "M", breeds.get("Tibetan Spaniel"), 5, 50.0, "24.png", 1600.00, true);
-            Dog dog25 = new Dog("25", "Yara", "F", breeds.get("Border Collie"), 6, 20.0, "25.png", 1000.00, true);
-            Dog dog26 = new Dog("26", "Zane", "M", breeds.get("Curly-Coated Retriever"), 3, 55.0, "26.png", 1300.00, true);
-            Dog dog27 = new Dog("27", "Amber", "F", breeds.get("Skye Terrier"), 4, 60.0, "27.png", 1500.00, true);
-            Dog dog28 = new Dog("28", "Ben", "M", breeds.get("Caucasian Shepherd Dog"), 2, 90.0, "28.png", 1800.00, true);
-            Dog dog29 = new Dog("29", "Cleo", "F", breeds.get("Hokkaido"), 5, 50.0, "29.png", 1200.00, true);
-            Dog dog30 = new Dog("30", "Duke", "M", breeds.get("Japanese Terrier"), 6, 40.0, "30.png", 1300.00, true);
+            Dog dog1 = new Dog("1", "Alexander", "M",
+                    breeds.get("Caucasian Shepherd Dog"), 24, 12.0, "1.png");
+            Dog dog2 = new Dog("2", "Bella", "F",
+                    breeds.get("Bouvier des Flandres"), 3, 65.0, "2.png",
+                    1200.00, true);
+            Dog dog3 = new Dog("3", "Charlie",
+                    "M", breeds.get("Hanoverian Scenthound"), 5,
+                    70.0, "3.png", 1500.00, true);
+            Dog dog4 = new Dog("4", "Daisy", "F",
+                    breeds.get("Tibetan Spaniel"), 4, 60.0,
+                    "4.png", 1300.00, true);
+            Dog dog5 = new Dog("5", "Ella", "F",
+                    breeds.get("Border Collie"), 2, 25.0,
+                    "5.png", 800.00, true);
+            Dog dog6 = new Dog("6", "Finn", "M",
+                    breeds.get("Curly-Coated Retriever"), 6, 50.0,
+                    "6.png", 900.00, true);
+            Dog dog7 = new Dog("7", "Ginger", "F",
+                    breeds.get("Skye Terrier"), 7, 55.0,
+                    "7.png", 1100.00, true);
+            Dog dog8 = new Dog("8", "Hunter", "M",
+                    breeds.get("Hokkaido"), 3, 85.0,
+                    "8.png", 1700.00, true);
+            Dog dog9 = new Dog("9", "Ivy", "F",
+                    breeds.get("Hokkaido"), 4, 45.0,
+                    "9.png", 1400.00, true);
+            Dog dog10 = new Dog("10", "Jack", "M",
+                    breeds.get("Japanese Terrier"), 5, 75.0,
+                    "10.png", 1600.00, true);
+            Dog dog11 = new Dog("11", "Kira", "F",
+                    breeds.get("Caucasian Shepherd Dog"), 6, 65.0,
+                    "11.png", 1200.00, true);
+            Dog dog12 = new Dog("12", "Leo", "M",
+                    breeds.get("Bouvier des Flandres"), 2, 20.0,
+                    "12.png", 700.00, true);
+            Dog dog13 = new Dog("13", "Molly", "F",
+                    breeds.get("Hanoverian Scenthound"), 7, 15.0,
+                    "13.png", 900.00, true);
+            Dog dog14 = new Dog("14", "Nala", "F",
+                    breeds.get("Tibetan Spaniel"), 5, 140.0,
+                    "14.png", 0, false);
+            Dog dog15 = new Dog("15", "Oscar", "M",
+                    breeds.get("Border Collie"), 3, 8.0,
+                    "15.png", 600.00, true);
+            Dog dog16 = new Dog("16", "Penny", "F",
+                    breeds.get("Curly-Coated Retriever"), 4, 5.0,
+                    "16.png", 800.00, true);
+            Dog dog17 = new Dog("17", "Quinn", "F",
+                    breeds.get("Skye Terrier"), 6, 12.0,
+                    "17.png", 1000.00, true);
+            Dog dog18 = new Dog("18", "Rocky", "M",
+                    breeds.get("Tibetan Spaniel"), 5, 60.0,
+                    "18.png", 1300.00, true);
+            Dog dog19 = new Dog("19", "Sophie", "F",
+                    breeds.get("Hokkaido"), 7, 7.0,
+                    "19.png", 850.00, true);
+            Dog dog20 = new Dog("20", "Toby", "M",
+                    breeds.get("Japanese Terrier"), 2, 30.0,
+                    "20.png", 1100.00, true);
+            Dog dog21 = new Dog("21", "Ursula", "F",
+                    breeds.get("Caucasian Shepherd Dog"), 3, 70.0,
+                    "21.png", 1500.00, true);
+            Dog dog22 = new Dog("22", "Victor", "M",
+                    breeds.get("Bouvier des Flandres"), 6, 80.0,
+                    "22.png", 1700.00, true);
+            Dog dog23 = new Dog("23", "Wendy", "F",
+                    breeds.get("Hanoverian Scenthound"), 4, 65.0, "23.png");
+            Dog dog24 = new Dog("24", "Xander", "M",
+                    breeds.get("Tibetan Spaniel"), 5, 50.0,
+                    "24.png", 1600.00, true);
+            Dog dog25 = new Dog("25", "Yara", "F",
+                    breeds.get("Border Collie"), 6, 20.0,
+                    "25.png", 1000.00, true);
+            Dog dog26 = new Dog("26", "Zane", "M",
+                    breeds.get("Curly-Coated Retriever"), 3, 55.0,
+                    "26.png", 1300.00, true);
+            Dog dog27 = new Dog("27", "Amber", "F",
+                    breeds.get("Skye Terrier"), 4, 60.0,
+                    "27.png", 1500.00, true);
+            Dog dog28 = new Dog("28", "Ben", "M",
+                    breeds.get("Caucasian Shepherd Dog"), 2, 90.0,
+                    "28.png", 1800.00, true);
+            Dog dog29 = new Dog("29", "Cleo", "F",
+                    breeds.get("Hokkaido"), 5, 50.0,
+                    "29.png", 1200.00, true);
+            Dog dog30 = new Dog("30", "Duke", "M",
+                    breeds.get("Japanese Terrier"), 6, 40.0,
+                    "30.png", 1300.00, true);
             model.addDog(dog1);
             model.addDog(dog2);
             model.addDog(dog3);
